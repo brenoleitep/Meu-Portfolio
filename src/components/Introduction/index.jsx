f717faf411b399d3af36781087db06e7b6b1f6f8
Breno Leite <brenoleitepereira@gmail.com>
2023-03-31 15:29:15 -0300

    Feat: Motion instalado

--- src/components/Introduction/index.jsx
+++ src/components/Introduction/index.jsx
@@ -9,31 +9,42 @@ import {
   StyledIntroduction,
   StyledLottieIntroduction,
 } from "./style";
+import { motion } from "framer-motion";
 
 const Introduction = () => {
   return (
     <StyledIntroduction>
-      <StyledDivisionIntroduction>
-        <StyledDivIntroduction>
-          <h2 data-aos="fade-up">
-            <Texts text="Olá, meu nome é" />
-          </h2>
-          <h1 data-aos="fade-up">
-            <Texts text="Breno Leite" />
-          </h1>
-          <h3 data-aos="fade-up">
-            <Texts text="Eu desenvolvo aplicações para a web." />
-          </h3>
-          <p data-aos="fade-up">
-            <Texts text="Eu sou um desenvolvedor web especializado em criar aplicações front-end para a web. Atualmente estou focado em construir landing pages e me tornar um desenvolvedor full-stack." />
-          </p>
-          <button data-aos="fade-up">Saiba mais sobre mim</button>
-        </StyledDivIntroduction>
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{
+          delay: 0.4,
+          x: { duration: 0.2 },
+          default: { ease: "easeIn" },
+        }}
+      >
+        <StyledDivisionIntroduction>
+          <StyledDivIntroduction>
+            <h2 data-aos="fade-up">
+              <Texts text="Olá, meu nome é" />
+            </h2>
+            <h1 data-aos="fade-up">
+              <Texts text="Breno Leite" />
+            </h1>
+            <h3 data-aos="fade-up">
+              <Texts text="Eu desenvolvo aplicações para a web." />
+            </h3>
+            <p data-aos="fade-up">
+              <Texts text="Eu sou um desenvolvedor web especializado em criar aplicações front-end para a web. Atualmente estou focado em construir landing pages e me tornar um desenvolvedor full-stack." />
+            </p>
+            <button data-aos="fade-up">Saiba mais sobre mim</button>
+          </StyledDivIntroduction>
 
-        <StyledLottieIntroduction data-aos="fade-up">
-          <Lottie animationData={dev} />
-        </StyledLottieIntroduction>
-      </StyledDivisionIntroduction>
+          <StyledLottieIntroduction data-aos="fade-up">
+            <Lottie animationData={dev} />
+          </StyledLottieIntroduction>
+        </StyledDivisionIntroduction>
+      </motion.div>
     </StyledIntroduction>
   );
 };
